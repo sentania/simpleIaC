73703542b4050b7f051d45283d2452e7b0014e63
Scott R. Bowe <scott@sentania.net>
2021-03-03 09:20:51 -0600

    ggg

--- deployments.tf
+++ deployments.tf
@@ -11,3 +11,17 @@ module deployment1 {
     refresh_token = var.refresh_token
     url = var.vra_url
 }
+
+module deployment2 {
+    source           = "./dynamicCentOS"
+    project_name = "LabSandbox"
+    blueprint_version = "demo1"
+    deployment_name = "deployment2"
+    serviceLevel = "sandbox"
+    serverSize = "Small"
+    vm_count = "2"
+    OS = "centOS7"
+    description = "deployment2"
+    refresh_token = var.refresh_token
+    url = var.vra_url
+}
