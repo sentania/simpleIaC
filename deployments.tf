f3172cd84fa4a80c9d378ffda97dedf41293a876
Scott R. Bowe <scott@sentania.net>
2021-03-03 09:47:43 -0600

    dfdf

--- deployments.tf
+++ deployments.tf
@@ -1,16 +1,3 @@
-module deployment1 {
-    source           = "./dynamicCentOS"
-    project_name = "LabSandbox"
-    blueprint_version = "demo1"
-    deployment_name = "deployment1"
-    serviceLevel = "sandbox"
-    serverSize = "Small"
-    vm_count = "2"
-    OS = "centOS7"
-    description = "deployment1"
-    refresh_token = var.refresh_token
-    url = var.vra_url
-}
 
 module deployment2 {
     source           = "./dynamicCentOS"
