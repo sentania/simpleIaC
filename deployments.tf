d6440855ba9a085d28c3d15c8650ec6e627ef1c4
Scott R. Bowe <scott@sentania.net>
2021-03-09 12:25:43 -0600

    add another

--- deployments.tf
+++ deployments.tf
@@ -25,3 +25,17 @@ module deployment2 {
     refresh_token = var.refresh_token
     url = var.vra_url
 }
+
+module deployment3 {
+    source           = "./dynamicCentOS"
+    project_name = "LabSandbox"
+    blueprint_version = "demo1"
+    deployment_name = "deployment2"
+    serviceLevel = "sandbox"
+    serverSize = "Small"
+    vm_count = "2"
+    OS = "centOS7"
+    description = "deployment2"
+    refresh_token = var.refresh_token
+    url = var.vra_url
+}
