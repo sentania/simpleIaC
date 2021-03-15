a8ae7c73b4b07b5476ba6bfce912586f203a2de7
Scott R. Bowe <scott@sentania.net>
2021-03-15 09:02:21 -0500

    .

--- basicDeployment.tf
+++ basicDeployment.tf
@@ -6,18 +6,6 @@ module deployment1 {
     description = "TF Deployment"
     serviceLevel = "sandbox"
     serverSize = "Small"
-    vm_count = "1"
-    OS = "centOS7"
-}
-
-module deployment2 {
-    source           = "./dynamicCentOS"
-    project_name = "LabSandbox"
-    blueprint_version = "LogInsight"
-    deployment_name = "deployment2"
-    description = "TF Deployment"
-    serviceLevel = "sandbox"
-    serverSize = "Small"
-    vm_count = "1"
+    vm_count = "2"
     OS = "centOS7"
 }
