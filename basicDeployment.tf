eac856b29b22379fd4530a2c4da566b04a2bcf74
Scott R. Bowe <scottb@sentania.net>
2021-03-15 21:32:03 -0500

    deploy hr app

--- basicDeployment.tf
+++ basicDeployment.tf
@@ -1,12 +1,26 @@
-module deployment2 {
+#### HR Application
+module HRDeployment1 {
     source           = "./basicIasVM"
     project_name = "Human Resources"
     catalog_item_version = "InitialRelease"
-    deployment_name = "HR 1"
+    deployment_name = "HR Sandbox"
     description = "TF Deployment"
-    serviceLevel = "production"
+    serviceLevel = "sandbox"
     serverSize = "Small"
     vm_count = "2"
     az = "az1"
 }
 
+module HRDeployment2 {
+    source           = "./basicIasVM"
+    project_name = "Human Resources"
+    catalog_item_version = "InitialRelease"
+    deployment_name = "HR Production"
+    description = "TF Deployment"
+    serviceLevel = "production"
+    serverSize = "Small"
+    vm_count = "2"
+    az = "az2"
+}
+
+#### End HR Application
