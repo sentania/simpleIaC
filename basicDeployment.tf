39418c7409256c0b32c77b36a8cfd1b80813d52b
Scott R. Bowe <scott@sentania.net>
2021-06-29 12:37:59 -0500

    labsandbox

--- basicDeployment.tf
+++ basicDeployment.tf
@@ -2,7 +2,7 @@
 ###My server
 module demo01 {
     source           = "./basicIasVM"
-    project_name = "Lab Sandbox"
+    project_name = "LabSandbox"
     catalog_item_version = "InitialRelease"
     deployment_name = "HR Sandbox"
     description = "TF Deployment"
