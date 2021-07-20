4348c1e0ef2602bc40027da5bdcf0fe4ba04a26a
Scott R. Bowe <scott@sentania.net>
2021-07-20 09:16:14 -0500

    Test PCI

--- basicDeployment.tf
+++ basicDeployment.tf
@@ -11,7 +11,7 @@ module demo01 {
     vm_count = "2"
     az = "az1"
 }
-###My server
+###Opencart Sandbox
 module sandboxOC {
     source           = "./openCart"
     project_name = "Opencart Application Group"
@@ -19,6 +19,22 @@ module sandboxOC {
     deployment_name = "oc-sandbox-tf"
     description = "oc-sandbox-tf"
     serviceLevel = "Sandbox"
+    cloudTarget = "vSphere"
+    clusterSize = "small"
+    size = "Small"
+    compliance = "pci"
+    username = "demouser"
+    password ="VMware1!"
+}
+
+###Opencart Test PCI
+module sandboxOC {
+    source           = "./openCart"
+    project_name = "Opencart Application Group"
+    catalog_item_version = "Release.1"
+    deployment_name = "oc-test-pci-tf"
+    description = "oc-test-pci-tf"
+    serviceLevel = "Test"
     cloudTarget = "vSphere"
     clusterSize = "small"
     size = "Small"
