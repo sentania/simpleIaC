52fca0108b8fb5e24d39400e1dae12b6d6bb6f63
Scott R. Bowe <scott@sentania.net>
2021-07-29 12:25:48 -0500

    disk space clean up

--- basicDeployment.tf
+++ basicDeployment.tf
@@ -11,34 +11,3 @@ module demo01 {
     vm_count = "2"
     az = "az1"
 }
-###Opencart Sandbox
-module sandboxOC {
-    source           = "./openCart"
-    project_name = "Opencart Application Group"
-    catalog_item_version = "Release.1"
-    deployment_name = "oc-sandbox-tf"
-    description = "oc-sandbox-tf"
-    serviceLevel = "Sandbox"
-    cloudTarget = "vSphere"
-    clusterSize = "small"
-    size = "Small"
-    compliance = "pci"
-    username = "demouser"
-    password ="VMware1!"
-}
-
-###Opencart Test PCI
-module testOpencart {
-    source           = "./openCart"
-    project_name = "Opencart Application Group"
-    catalog_item_version = "Release.1"
-    deployment_name = "oc-test-pci-tf"
-    description = "oc-test-pci-tf"
-    serviceLevel = "Test"
-    cloudTarget = "vSphere"
-    clusterSize = "small"
-    size = "Small"
-    compliance = "pci"
-    username = "demouser"
-    password ="VMware1!"
-}
