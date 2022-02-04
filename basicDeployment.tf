51e47f27614dbf299f0817fc2480bee7eb9cb2d3
Scott R. Bowe <scott@sentania.net>
2022-02-04 13:11:25 -0600

    asdfsadf

--- basicDeployment.tf
+++ basicDeployment.tf
@@ -13,3 +13,19 @@ module sandboxOC {
     username = "demouser"
     password ="VMware1!"
 }
+
+###Opencart Sandbox
+module sandboxOC {
+    source           = "./openCart"
+    project_name = "Opencart Application Group"
+    catalog_item_version = "Release.3"
+    deployment_name = "oc-sandbox-t1f"
+    description = "oc-sandbox-tf"
+    serviceLevel = "Sandbox"
+    cloudTarget = "vSphere"
+    clusterSize = "small"
+    size = "Small"
+    compliance = "pci"
+    username = "demouser"
+    password ="VMware1!"
+}
