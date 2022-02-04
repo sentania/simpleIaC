8a0c7999c28fc419f7199555d98474d35584c90e
Scott R. Bowe <scott@sentania.net>
2022-02-04 11:51:29 -0600

    cleanup

--- basicDeployment.tf
+++ basicDeployment.tf
@@ -13,19 +13,3 @@ module sandboxOC {
     username = "demouser"
     password ="VMware1!"
 }
-
-###Opencart Test PCI
-module testOpencart {
-    source           = "./openCart"
-    project_name = "Opencart Application Group"
-    catalog_item_version = "Release.3"
-    deployment_name = "oc-test-pci-tf"
-    description = "oc-test-pci-tf"
-    serviceLevel = "Test"
-    cloudTarget = "vSphere"
-    clusterSize = "small"
-    size = "Small"
-    compliance = "pci"
-    username = "demouser"
-    password ="VMware1!"
-}
