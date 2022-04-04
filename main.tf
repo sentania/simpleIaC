74d769af037be3a7cbd942b2d1a2d2b3c4d687aa
Scott R. Bowe <scott@sentania.net>
2022-04-04 12:26:11 -0500

    test

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ provider "vra" {
   refresh_token = var.refresh_token
   insecure      = var.insecure
 }
+###Opencart Sandbox
+module simpleIACSandbox {
+    source           = "./simpleIaaS"
+    project_name = "LabSandbox"
+    catalog_item_version = "CentOS8"
+    deployment_name = "simpleIaaS"
+    description = "simple iaas"
+    serviceLevel = "sandbox"
+    az = "az1"
+    vm_count = "2"
+    serverSize = "Small"
+}
