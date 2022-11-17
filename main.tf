01aa966ebe7ace6061ecadf7ffa47b0f454bd487
Scott R. Bowe <scott@sentania.net>
2022-11-17 10:03:47 -0600

    Correct Deployment

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ module nginxServer01 {
 module nginxServer02 {
     source           = "./nginx"
     project_name = "Private Cloud Sandbox"
-    catalog_item_version = "ProdWebServer2"
-    deployment_name = "Nginx Provisioned by TF"
+    catalog_item_version = "ProdWebServer"
+    deployment_name = "Nginx Provisioned by TF 2"
     description = "Provisioned by TF"
     serviceLevel = "production"
     storagetier = "vsan"
